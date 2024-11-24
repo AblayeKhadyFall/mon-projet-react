24ca096dbcfa3a072a171172f0ebb4e309efccec
Ablaye Khady Fall <fallenbi02@gmail.com>
2024-11-24 01:04:19 +0000

    cote&epertice blog en react

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+      <div>
+        <a className="logo" href="">Code&Expertice blog</a>
+      </div>
+      <ul className="liens">
+        <li>
+          <a href=""className="lien">Accueil</a>
+        </li>
+        <li>
+          <a href="" className="lien buttonArticle" >Creer un article</a>
+        </li>
+       
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
